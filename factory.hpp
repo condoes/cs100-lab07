ef770d08d0a022fb5e9a35f151b566167b15628b
galva041 <galva041@ucr.edu>
2020-12-01 00:59:16 -0800

    factory.hpp fixed

--- factory.hpp
+++ factory.hpp
@@ -1,6 +1,6 @@
-#ifndef _factory_hpp_
-#define _factory_hpp_
-
+#ifndef __FACTORY_HPP__
+#define __FACTORY_HPP__
+#include <vector>
 #include "lab-04---composite-pattern-cdo032-galva041-lab4/add.hpp"
 #include "lab-04---composite-pattern-cdo032-galva041-lab4/Mult.hpp"
 #include "lab-04---composite-pattern-cdo032-galva041-lab4/sub.hpp"
@@ -8,11 +8,14 @@
 #include "lab-04---composite-pattern-cdo032-galva041-lab4/pow.hpp"
 #include "lab-04---composite-pattern-cdo032-galva041-lab4/base.hpp"
 #include "lab-04---composite-pattern-cdo032-galva041-lab4/Div.hpp"
-#include <vector>
+
 
 class Factory {
     public:
-        Base* parse(char** input, int length);
+    Base* parse(char** input, int length);
 };
 
-#endif //_factory_hpp_
+
+
+
+#endif
