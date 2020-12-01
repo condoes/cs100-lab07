789845d5ef3174952cb211ad23da46bbff858e24
Cindy Do <cindy.do10@gmail.com>
2020-11-30 20:56:54 -0800

    modified cmake and main

--- main.cpp
+++ main.cpp
@@ -5,22 +5,15 @@
 #include "lab-04---composite-pattern-cdo032-galva041-lab4/op.hpp"
 #include "lab-04---composite-pattern-cdo032-galva041-lab4/pow.hpp"
 #include "lab-04---composite-pattern-cdo032-galva041-lab4/base.hpp"
+#include "factory.hpp"
 
 using namespace std;
 
 int main (int argc, char ** argv) {
 
-    Base* three = new Op(3);
-    Base* seven = new Op(7);
-    Base* four = new Op(4);
-    Base* two = new Op(2);
-    Base* mult = new Mult(seven, four);
-    Base* add = new Add(three, mult);
-    Base* minus = new Sub(add, two);
-
-    cout << minus->stringify() << " = " << minus->evaluate() << std::endl;
-    return 0;
-
+    Factory* fact = new Factory();
+    Base* temp = fact->parse(argv, argc);
+    cout << temp->stringify() << " = " << temp->evaluate() << endl;
 
     return 0;
 }
